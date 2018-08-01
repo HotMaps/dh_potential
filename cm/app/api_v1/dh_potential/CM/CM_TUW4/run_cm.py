dcc81ec7f1778bbd8724fd9d2756efeb1d83f2ec
fallahnejad <fallahnejad@eeg.tuwien.ac.at>
2018-08-01 11:49:19 +0200

    dh potential

---
+++ cm/app/api_v1/dh_potential/CM/CM_TUW4/run_cm.py
@@ -0,0 +1,56 @@
+import os
+import time
+import sys
+path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.
+                                                       abspath(__file__))))
+if path not in sys.path:
+    sys.path.append(path)
+from CM.CM_TUW4.polygonize import polygonize
+import CM.CM_TUW4.district_heating_potential as DHP
+import CM.CM_TUW19.run_cm as CM19
+
+
+def main(heat_density_map, region, pix_threshold, DH_threshold, output_dir,
+         in_orig=None, only_return_areas=False):
+    DH_Regions, clipped_hdm, rasterOrigin = DHP.DHReg(heat_density_map, region,
+                                                      pix_threshold,
+                                                      DH_threshold,
+                                                      in_orig)
+    if only_return_areas:
+        clipped_hdm = rasterOrigin = None
+        return DH_Regions
+    """
+    Outputs:
+        DH_Regions: contains binary values (no units) showing coherent areas
+        clipped_hdm: hdm after applying pixel threshold [MWh/ha]
+        rasterOrigin: Coordination of the top-left corner of raster
+    """
+    outRasterPath1 = output_dir + os.sep + 'F13_' + 'Pot_areas.tif'
+    outRasterPath2 = output_dir + os.sep + 'F13_' + 'labels.tif'
+    outShapefilePath = output_dir + os.sep + 'F13_' + 'Pot_AT_TH30.shp'
+    DHPot, labels = DHP.DHPotential(DH_Regions, clipped_hdm)
+    
+    """potential of each coherent area in GWh is assigned to its pixels"""
+    geo_transform = [rasterOrigin[0], 100, 0, rasterOrigin[1], 0, -100]
+    CM19.main(outRasterPath1, geo_transform, 'float64', DH_Regions)
+    CM19.main(outRasterPath2, geo_transform, 'int32', labels)
+    polygonize(outRasterPath1, outRasterPath2, outShapefilePath, DHPot)
+    return outRasterPath1, outShapefilePath
+
+
+if __name__ == "__main__":
+    start = time.time()
+    data_warehouse = path + os.sep + 'AD/data_warehouse'
+    heat_density_map = data_warehouse + os.sep + 'heat_tot_curr_density_AT.tif'
+    region = data_warehouse + os.sep + 'AT.shp'
+    output_dir = path + os.sep + 'Outputs'
+    outRasterPath = output_dir + os.sep + 'F13_' + 'Pot_AT_TH30.tif'
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    # pix_threshold [GWh/km2]
+    pix_threshold = 10
+    # DH_threshold [GWh/a]
+    DH_threshold = 30
+    main(heat_density_map, region, pix_threshold, DH_threshold, outRasterPath)
+    elapsed = time.time() - start
+    print("%0.3f seconds" % elapsed)
---
+++ cm/app/api_v1/dh_potential/FEAT/F13_district_heating_potential/F_13.py
@@ -0,0 +1,41 @@
+import os
+import sys
+path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.
+                                                       abspath(__file__))))
+if path not in sys.path:
+    sys.path.append(path)
+from AD.F13_district_heating_potential.main import ad_f13
+import CM.CM_TUW4.run_cm as CM4
+
+
+
+def execute(output_dir):
+    '''
+    hdm in raster format from default dataset
+    pix_threshold [GWh/km2]
+    DH_threshold [GWh/a]
+    potential level: NUTS0, 1, 2, 3, own shapefile
+    '''
+    heat_density_map, strd_vector, pix_threshold, DH_threshold = ad_f13()
+    '''
+    Shapefile of NUTS0, 1, 2, 3 should be available in data warehouse; User
+    should also be able to calculate potential for the selected area: shall
+    be considered in AD
+    Outputs:
+        DH_Regions: contains binary values (no units) showing coherent areas
+        clipped_hdm: hdm after clipping [MWh/ha]
+        rasterOrigin: Top-left corner of raster array
+    '''
+    outRasterPath, outShapefile = CM4.main(heat_density_map, strd_vector,
+                                           pix_threshold, DH_threshold,
+                                           output_dir)
+    return {'F13_out_raster_path_0': outRasterPath,
+            'F13_out_shapefile_path_0': outShapefile}
+
+
+if __name__ == "__main__":
+    output_dir = path + os.sep + 'Outputs'
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    output = execute(output_dir)
+    print(output)
