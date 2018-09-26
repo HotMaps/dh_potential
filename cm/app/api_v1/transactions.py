951aae779749239e2261ad6aea64abf15492df9d
lesly houndole <lesly.houndole@crem.ch>
2018-09-26 18:08:02 +0200

    update

--- cm/app/api_v1/transactions.py
+++ cm/app/api_v1/transactions.py
@@ -136,6 +136,7 @@ def compute():
     inputs_parameter_selection = helper.validateJSON(data["inputs_parameter_selection"])
     print ('inputs_parameter_selection', inputs_parameter_selection)
     LOGGER.info('inputs_parameter_selection', inputs_parameter_selection)
+
     output_directory = UPLOAD_DIRECTORY
     # call the calculation module function
     result = calculation_module.calculation(output_directory, inputs_raster_selection,inputs_parameter_selection)
@@ -145,7 +146,7 @@ def compute():
     print("response ",response)
     LOGGER.info('response', response)
     print("type response ",type(response))
-    LOGGER.info("type response ",type(response))
+
     # convert response dict to json
     response = json.dumps(response)
     return response
